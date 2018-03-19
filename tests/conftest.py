a0c19f5592630c77b47e3d9bdee6489ee7f98d37
travis-ci <travis@travis-ci.org>
2018-03-19 00:25:07 +0000

    Deploy Travis CI build 6 to GitHub

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+"""Integration tests configuration file."""
+
+from demo_project.tests.conftest import pytest_configure  # pylint: disable=unused-import
+
+from .fixtures import *  # pylint: disable=wildcard-import,unused-wildcard-import
